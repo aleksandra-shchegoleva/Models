ce1fb968836c4261f463e83e4a419da815965a79
РђР»РµРєСЃР°РЅРґСЂР° Р©РµРіРѕР»РµРІР° <aleksandrasheg@yandex.ru>
2020-11-17 20:40:04 +0300

    system b

--- Real_data/System_B.m
+++ Real_data/System_B.m
@@ -1,48 +1,50 @@
-N = 154; %период
-h = 1; %шаг
+N = 154; %РїРµСЂРёРѕРґ
+h = 1; %С€Р°Рі
 
-M = 0:h:N; %сетка времени
+M = 0:h:N; %СЃРµС‚РєР° РІСЂРµРјРµРЅРё
 
 x1 = zeros(1, length(M));
 x2 = zeros(1, length(M));
 alpha1 = zeros(1, length(M));
 U(1) = 0;
 
-e1 = 10^3; %критерий устойчивости
+e1 = 10^3; %РєСЂРёС‚РµСЂРёР№ СѓСЃС‚РѕР№С‡РёРІРѕСЃС‚Рё
 flag = 0;
 real_data(:,1) = xlsread("data.xlsx", 'H3:H5');
 real_data(:,2) = xlsread("data.xlsx", 'J3:J5');
 real_data(:,1) = real_data(:,1)./100000;
 real_data(:,2) = real_data(:,2)./1000;
 
-k = 10;
+k = 25;
 B = 20;
 xc = real_data(1, 1).*k;
-e2 = .01 * xc; %критерий достижения цели
-x1(1) = real_data(1, 1); %начальные условия численности фитопланктона
-x2(1) = real_data(1, 2); %начальные условия численности зоопланктона
+e2 = .01 * xc; %РєСЂРёС‚РµСЂРёР№ РґРѕСЃС‚РёР¶РµРЅРёСЏ С†РµР»Рё
+x1(1) = real_data(1, 1); %РЅР°С‡Р°Р»СЊРЅС‹Рµ СѓСЃР»РѕРІРёСЏ С‡РёСЃР»РµРЅРЅРѕСЃС‚Рё С„РёС‚РѕРїР»Р°РЅРєС‚РѕРЅР°
+x2(1) = real_data(1, 2); %РЅР°С‡Р°Р»СЊРЅС‹Рµ СѓСЃР»РѕРІРёСЏ С‡РёСЃР»РµРЅРЅРѕСЃС‚Рё Р·РѕРѕРїР»Р°РЅРєС‚РѕРЅР°
+x1(1) = x1(1) - xc / 2;
+x2(1) = x2(1) - B / 2;
 
 mas = [];
 mas_stab = [];
 data = [];
 max_stab = 0;
 flag_find = 0;
-T2_min = 1000;
-T2_max = 10000;
+T2_min = 0;
+T2_max = 1000000;
 
-% alpha1 = 0.32;
-% alpha2 = 0.08;
-% beta1 = 0.018;
-% beta2 = 0.229;
+% alpha1(1) = 0.12;
+% alpha2 = 0.06;
+% beta1 = 0.002;
+% beta2 = 0.082;
 
-for a1 = 0.5:0.1:1
+for a1 = 0.1:0.1:1
     alpha1(1) = a1;
-for alpha2 = 0.0001:0.0001:0.5
-for beta1 = 0.01:0.01:0.8
-for beta2 = 0.00001:0.00001:0.0005
+for alpha2 = 0.01:0.01:1
+for beta1 = 0.0001:0.0001:0.1
+for beta2 = 0:0.00005:0.001
 % for B = 100:10:200
-for T1 = 0.01:0.01:10
-for T2 = T2_min:10^6
+for T1 = 0:1:100000
+for T2 = T2_min:T2_max
 for n=1:(length(M) - 1)
       f1 = alpha1(n)*x1(n) - beta1*x1(n)*x2(n);
        f2 = -alpha2*x2(n) + beta2*x1(n)*x2(n);
@@ -63,23 +65,35 @@ for n=1:(length(M) - 1)
        x2(n + 1) = x2(n) + h*f2;
        alpha1(n + 1) = alpha1(n) + h*f3;
        
-        if abs(x1(n + 1)) >= e1 || x1(n + 1) < 0
+        if abs(x1(n + 1)) >= 2*xc
            flag = 1;
+           if n > max_stab
+              max_stab = n; 
+              disp([T1 T2]);
+           end
             break
          end
-        if abs(x2(n + 1)) >= e1 || x2(n + 1) < 0
+        if abs(x2(n + 1)) > B
            flag = 1;
+           if n > max_stab
+              max_stab = n; 
+              disp([T1 T2]);
+           end
            break
         end
-         if abs(alpha1(n + 1)) >= e1 || alpha1(n + 1) < 0
+         if abs(alpha1(n + 1)) >= e1
             flag = 1;
+            if n > max_stab
+              max_stab = n; 
+              disp([T1 T2]);
+           end
             break
          end
 end
-    if flag == 0 && T1 ~= 0 && T2 ~= 0
+    if flag == 0 && T1 ~= 0 && T2 ~= 0 && beta2 ~= 0
         mas_stab(end + 1, :) = [k B T1 T2 alpha1(1) alpha2 beta1 beta2];
         flag_find = 1;
-%         disp([k B T1 T2 alpha1(1) alpha2 beta1 beta2]);
+        disp([k B T1 T2 alpha1(1) alpha2 beta1 beta2]);
 %     for i=1:100
 %        if abs(x1(i) - xc) <= e2 && abs(x1(i + 1) - xc) <= e2 && abs(x1(100) - xc) <= e2
 %             mas(end + 1, :) = [k i T1 T2 alpha1(1) alpha2 beta1 beta2];
@@ -95,7 +109,7 @@ end
 end
     if flag_find == 1
         flag_find = 0;
-        T2_min = T2_min + 1000;
+%         T2_min = T2_min + 1000;
         T2_max = T2_max + 5000;
        break; 
     end
